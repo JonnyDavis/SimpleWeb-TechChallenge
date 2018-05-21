a043f1a69b406a86c50670fcfd692ac7466cbee9
Jonny Davis <32075941+JonnyDavis@users.noreply.github.com>
2018-05-22 00:08:41 +0100

    Delete settings.js

--- src/config/settings.js
+++
@@ -1,5 +0,0 @@
-export const OAUTH_CLIENT_ID = 2
-export const OATUH_CLIENT_SECRET = '8yORFgKDTxcwy21kQA9yMvZO9gxQLsDGvtm5pFA5'
-
-// export const OAUTH_CLIENT_ID = 2
-// export const OAUTH_CLIENT_SECRECT = '4Cdq2mwWrbVLG2WaJHxhGYuciJVNWk5dVMSLoZX8'
